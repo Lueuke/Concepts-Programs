6b356ec65b06e32730dfb21d943845fd9d4f851e
Luke <lukedekan@gmail.com>
2023-11-17 08:53:15 -0600

    Serial

--- Serial.py
+++ Serial.py
@@ -5,6 +5,11 @@ import argparse
 parser = argparse.ArgumentParser()
 args = parser.parse_args()
 
+def validate_seed_string(seed_string):
+    if not all(char in 'abc' for char in seed_string):
+        raise argparse.ArgumentTypeError("Seed string must consist of lowercase letters a, b, and c.")
+    return seed_string
+
 parser.add_argument('-i', '--input', type=validate_file_path, help='Path to input file', required=True)
 parser.add_argument('-s', '--seed', type=validate_seed_string, help='Seed string', required=True)
 parser.add_argument('-o', '--output', type=validate_output_path, help='Path to output file', required=True)
@@ -12,11 +17,6 @@ parser.add_argument('-p', '--processes', type=validate_positive_integer, default
 
 input_file = args.input
 seed_string = args.seed
-
-def validate_seed_string(seed_string):
-    if not all(char in 'abc' for char in seed_string):
-        raise argparse.ArgumentTypeError("Seed string must consist of lowercase letters a, b, and c.")
-    return seed_string
 
 
 def create_matrix(input_file):
